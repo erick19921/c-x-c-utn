c618ca1734b2823b8b9eb9ee872deb4b29b20fe5
erick19921 <51097323+erick19921@users.noreply.github.com>
2021-01-27 16:03:29 -0500

    update cnn

--- cnn.js
+++ cnn.js
@@ -1,10 +1,10 @@
 const pgPromise = require("pg-promise")
 const config = {
-    host:"localhost",
+    host:"ec2-54-205-248-255.compute-1.amazonaws.com",
     port:"5432",
-    database:"respaldo",
-    user:"postgres",
-    password:"3434"
+    database:"d2ekg96sn2m8uu",
+    user:"zttsxgiuobpkns",
+    password:"5e1874412c937635995cc24c5eb96ec4fbc128c7469e64ffb42567650882891d"
 }
 const pgp = pgPromise({})
 const db = pgp(config)
@@ -13,6 +13,7 @@ const db = pgp(config)
 db.any("select * from factura").then(res => {console.log(res)})
 db.any("select * from pagos_clientes").then(res => {console.log(res)})
 db.any("select * from tipos_pago").then(res=>{console.log(res)})
-db.any("select * from usuario").then(res=>{console.log(res)})
+//db.any("select * from usuario").then(res=>{console.log(res)})
 
 exports.db=db;
+
